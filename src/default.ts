a5451d00f4d969ed480064eb75bf19f015bde2bd
carrainme <130687012+carrainme@users.noreply.github.com>
2023-05-08 17:48:25 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -8,4 +8,4 @@ export const defaultSetting = {
 
 export const defaultMessage = `
 
-欢迎来到待开的网页，本页面只做chatgpt问答转发，仅供娱乐和学习，填入你的apikey，然后输入你的问题吧`
+欢迎来到待开的网页，本页面只做chatgpt问答转发，仅供娱乐和学习，点击设置，填入你的apikey，然后输入你的问题吧`
